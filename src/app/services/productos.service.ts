7f270c8efa68cf5e26d576991b0fdc5ac80b070f
Matías Henning <matias.henning@gmail.com>
2019-07-04 12:39:45 -0400

    Fin sección 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,6 +9,7 @@ export class ProductosService {
 
 	cargando = true;
 	productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor(private http: HttpClient) { 
 
@@ -17,19 +18,57 @@ export class ProductosService {
   }
   private cargarProductos(){
 
-  	this.http.get('https://portafolio-7b9ea.firebaseio.com/productos_idx.json')
-  	.subscribe((resp: Producto[]) => {
+    return new Promise((resolve, reject) => {
+      this.http.get('https://portafolio-7b9ea.firebaseio.com/productos_idx.json')
+      .subscribe((resp: Producto[]) => {
+        this.productos = resp;
+        this.cargando = false;
+        resolve();
+      });
+    });
 
 
-  		this.productos = resp;
-      this.cargando = false;
-
-  	});
 
   }
 
   getProducto( id: string ){
     return this.http.get(`https://portafolio-7b9ea.firebaseio.com/productos/${ id }.json`);
   }
+
+  buscarProducto( termino: string ){
+
+    if (this.productos.length == 0) {
+      this.cargarProductos().then(() => {
+        //ejecuta despues de tener los productos
+      });
+    } else{
+      //despues de aplicar filtros
+      this.filtrarProductos( termino );
+    }
+
+    this.productosFiltrado.filter( producto => {
+      return true;
+    });
+  console.log(this.productosFiltrado);
+  }
+
+  private filtrarProductos( termino: string ){
+
+    console.log(this.productos);
+    this.productosFiltrado = [];
+
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach( prod => {
+
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+
+      if (prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0) {
+        this.productosFiltrado.push( prod );
+      }
+    });
+
+  }
+
 
 }
