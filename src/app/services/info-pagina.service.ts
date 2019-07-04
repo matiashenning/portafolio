ef3b4e23209ffa2b3a160e82caed3cd1f524575c
Matías Henning <matias.henning@gmail.com>
2019-07-04 00:26:54 -0400

    Fin sección 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -7,22 +7,36 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 })
 export class InfoPaginaService {
 
-	info: InfoPagina = {};
+  info: InfoPagina = {};
 	cargada = false;
+
+  equipo: any[] = [];
 
   constructor(private http: HttpClient) { 
 
-  	console.log("info pagina servicio");
-
-  	// Leer JSON
-  	this.http.get('assets/data/data-pagina.json')
-  	.subscribe( (resp: InfoPagina) => {
-
-  		this.cargada = true;
-  		this.info = resp;
-  		console.log(resp['email']);
-
-  	});
+    this.cargarInfo();
+    this.cargarEquipo();
 
   }
+
+  private cargarInfo(){
+    // Leer JSON
+    this.http.get('assets/data/data-pagina.json')
+    .subscribe( (resp: InfoPagina) => {
+
+      this.cargada = true;
+      this.info = resp;
+    });
+  }
+
+  private cargarEquipo(){
+    // Leer JSON
+    this.http.get('https://portafolio-7b9ea.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) => {
+
+      this.equipo = resp;
+      //console.log(resp);
+    });
+  }
+
 }
